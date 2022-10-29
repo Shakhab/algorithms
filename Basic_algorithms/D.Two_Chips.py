a4b6c9708b24ea46bad2d461a9b579f6dfc06dd0
Shakhab <shakhabkhan1916@gmail.com>
2022-10-30 01:41:26 +0300

    fix_chips_alg

--- Basic_algorithms/D.Two_Chips.py
+++ Basic_algorithms/D.Two_Chips.py
@@ -1,14 +1,12 @@
 def chips(n, a, x):
-    m = ''
     for i in range(n - 1):
         for j in range(i + 1, n):
-            b = a[i] + a[j]
-            if b == x:
-                m += str(a[i]) + ' ' + str(a[j])
-                return ''.join(m)
-    return None
+            if a[i] + a[j] == x:
+                return a[i], a[j]
+    return [None]
 
 n = int(input())
 a = list(map(int, input().strip().split()))
 x = int(input())
-print(chips(n, a, x))
+print(*chips(n, a, x))
+
